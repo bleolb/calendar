b6297b8239e313c7c34c84a7f33b2c1bd206e318
unknown <ble.ona@yavirac.edu.ec>
2019-07-15 18:54:05 -0500

    Dome

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { DemoComponent } from './app.component';
+import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
+
+@NgModule({
+  imports: [
+    ScheduleModule, 
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    
+      
+    })
+  ],
+  declarations: [DemoComponent],
+  exports: [DemoComponent]
+})
+export class DemoModule {}
